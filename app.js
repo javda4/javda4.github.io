8449a0c441f37e64a01c31bf23cc23ddc52324c4
Joshua Van Doren <72502663+javda4@users.noreply.github.com>
2026-04-13 23:45:32 -0700

    Update app.js

--- app.js
+++ app.js
@@ -35,3 +35,22 @@ setInterval(() => {
 }, 1000);
 
 startCamera();
+
+const testCanvas = document.createElement("canvas");
+const ctx = testCanvas.getContext("2d");
+
+function checkFrame() {
+  if (video.readyState === 4) {
+
+    testCanvas.width = video.videoWidth;
+    testCanvas.height = video.videoHeight;
+
+    ctx.drawImage(video, 0, 0);
+
+    const frame = ctx.getImageData(0, 0, 10, 10);
+    debug.innerText =
+      "frame OK ✔ pixel[0]=" + frame.data[0];
+  }
+}
+
+setInterval(checkFrame, 500);
